3f7fcc408f36a7688b8e32b4b7b62c56a1117ede
AbdurRahmanSalami <abdursalamioluwa@gmail.com>
2024-05-23 18:01:45 +0100

    Logical operations

--- index.js
+++ index.js
@@ -46,3 +46,11 @@ a >= 10; //Returns true
 a <= 20; //Returns true 
 a == 10; //Returns true 
 a === "10"; //Returns false because a holds an integer value of 10 and not string value "10" 
+
+a = 1; 
+b = 2; 
+console.log(a < 3 && b < 3); //Prints true as both a and b contain values that are less than 3. 
+console.log(a > 3 && b < 3); //Prints false as the first condition (a > 3) is not true. 
+console.log(a > 3 || b < 3); //Prints true as at least one condition (b < 3) is true. 
+console.log(!(a < 3)); //Prints false as condition (NOT a < 3) is false. 
+console.log(!(a < 3) || !(b > 3)); //Prints true as one of the conditions (NOT b > 3) is true. 
